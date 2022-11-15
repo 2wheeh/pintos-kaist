4fc7fab7031dd3a0b35276ff50ef051c549f37ff
hoon25 <cchoon95@hanmail.net>
2022-11-15 04:27:14 +0000

    success priority-sema

--- threads/synch.c
+++ threads/synch.c
@@ -66,7 +66,8 @@ sema_down (struct semaphore *sema) {
 
 	old_level = intr_disable ();
 	while (sema->value == 0) {
-		list_push_back (&sema->waiters, &thread_current ()->elem);
+		// list_push_back (&sema->waiters, &thread_current ()->elem);
+		list_insert_ordered(&sema->waiters, &thread_current()->elem, check_a_bigger_b, NULL);
 		thread_block ();
 	}
 	sema->value--;
@@ -109,10 +110,16 @@ sema_up (struct semaphore *sema) {
 	ASSERT (sema != NULL);
 
 	old_level = intr_disable ();
-	if (!list_empty (&sema->waiters))
+	if (!list_empty (&sema->waiters)){
+		// sema내부에서 priority 변경되었을 수도 있으므로 한번더 정렬
+		list_sort(&sema->waiters, check_a_bigger_b, NULL);
 		thread_unblock (list_entry (list_pop_front (&sema->waiters),
 					struct thread, elem));
+	}
 	sema->value++;
+
+	// thread_yield()가 thread_unblock()으로 가면 오류가 발생함 --> 확인 필요
+	thread_yield();
 	intr_set_level (old_level);
 }
 
