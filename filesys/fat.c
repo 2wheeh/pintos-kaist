1b02bd780a6f71416846a55e4a223e0eb5172fe5
Ubuntu <ubuntu@ip-172-31-11-188.ap-northeast-2.compute.internal>
2022-12-16 11:44:16 +0000

    filesys-first-commit

--- filesys/fat.c
+++ filesys/fat.c
@@ -153,6 +153,8 @@ fat_boot_create (void) {
 void
 fat_fs_init (void) {
 	/* TODO: Your code goes here. */
+	fat_fs->fat_length = fat_fs->bs.fat_sectors * DISK_SECTOR_SIZE / (sizeof(cluster_t)) *SECTORS_PER_CLUSTER;
+	fat_fs->data_start = fat_fs->bs.fat_start + fat_fs->bs.fat_sectors;
 }
 
 /*----------------------------------------------------------------------------*/
@@ -165,6 +167,28 @@ fat_fs_init (void) {
 cluster_t
 fat_create_chain (cluster_t clst) {
 	/* TODO: Your code goes here. */
+	cluster_t i = 2;
+	while (fat_get(i) != 0 && i < fat_fs->fat_length) {
+		++i;
+	}
+	
+	if (i == fat_fs->fat_length) {	// FAT가 가득 찼다면
+		return 0;
+	}
+	
+	fat_put(i, EOChain);	// fat안의 값 업데이트
+
+	if (clst == 0) {	// 새로운 체인 생성
+		return i;
+	}
+
+	while(fat_get(clst) != EOChain) {
+		clst = fat_get(clst);
+	}
+
+	fat_put(clst, i);
+	return i;
+
 }
 
 /* Remove the chain of clusters starting from CLST.
@@ -178,16 +202,19 @@ fat_remove_chain (cluster_t clst, cluster_t pclst) {
 void
 fat_put (cluster_t clst, cluster_t val) {
 	/* TODO: Your code goes here. */
+	fat_fs->fat[clst] = val;
 }
 
 /* Fetch a value in the FAT table. */
 cluster_t
 fat_get (cluster_t clst) {
 	/* TODO: Your code goes here. */
+	return fat_fs->fat[clst];
 }
 
 /* Covert a cluster # to a sector number. */
 disk_sector_t
 cluster_to_sector (cluster_t clst) {
 	/* TODO: Your code goes here. */
+	return fat_fs->data_start + clst;
 }
